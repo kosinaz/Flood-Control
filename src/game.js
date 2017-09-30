65ea3a48dc3b02fe554efac09ada2d489440d7f1
Zoli <zoltan@kosina.hu>
2017-09-30 19:33:40 +0200

    Flood the border

--- src/game.js
+++ src/game.js
@@ -10,6 +10,7 @@ function preload() {
 var levels;
 var buildings;
 var streets;
+var water;
 var graphics;
 var timer;
 
@@ -24,6 +25,7 @@ var cityY = 180;
 var barricades = 3;
 var barricadeCounter;
 var countdownTimer;
+var startTime = 6;
 
 function create() {
     
@@ -38,7 +40,7 @@ function create() {
  
     // A timer to count the seconds before the flood
     timer = game.time.create(true);
-    timer.loop(7000, flood, this);
+    timer.loop(startTime * 1000, flood, this);
     timer.start();
 
     // Display how many seconds left to build
@@ -71,6 +73,9 @@ function create() {
     
     // The buildings group contains the initial barricades of the flood
     buildings = game.add.group();
+    
+    // The water that floods the streets
+    water = game.add.group();
 
     // Build all the assets of the current level data
     for (x = 0; x < levels[0].length; x += 1) {
@@ -90,7 +95,7 @@ function create() {
             } else if (x % 2 || y % 2) {
 
                 // Build a street suitable for a barricade on every second tile
-                streets.add(game.add.button(tileX, tileY, 'tileset', actionOnClick, this, 17, 1, 18));
+                streets.add(game.add.button(tileX, tileY, 'tileset', buildBarricade, this, 17, 1, 18));
 
             } else {
 
@@ -104,20 +109,21 @@ function create() {
 function update() {
 
     // Update how many seconds left to build
-    countdownTimer.text = '0:0' + Math.floor(timer.duration / 1000);
+    countdownTimer.text = '0:0' + Math.ceil(timer.duration / 1000);
 
     // Update the countdown timer bar of the flood
     graphics.beginFill(0x0000ff);
-    graphics.drawRect(200, 40, Math.min(timer.ms / 6000 * 400, 400), 40);
+    graphics.drawRect(200, 40, Math.min(timer.ms / startTime * 0.4, 400), 40);
     graphics.endFill();
 }
 
-function actionOnClick() {
+function buildBarricade() {
     
     // Build a barricade on the selected street
     buildings.create(arguments[0].x, arguments[0].y, 'tileset', 18);
+    levels[0][(arguments[0].x - cityX) / 40][(arguments[0].y - cityY) / 40] = 2;
     
-    // Make sure that the barricades will be displayed above the street
+    // Make sure that the barricades will be displayed on top of the street
     game.world.bringToTop(buildings);
 
     // Decrease the number of buildable barricades
@@ -136,4 +142,52 @@ function actionOnClick() {
 
 function flood() {
 
+    // Stop the timer to start the flood
+    timer.stop();
+
+    // Set water sources in every intersections of the edge of the level
+    for (x = 0; x < levels[0].length; x += 2) {
+
+        // The x position of the current tile
+        tileX = cityX + x * tileWidth;
+        
+        // The y position of the current tile
+        tileY = cityY;
+
+        // Set the top border
+        levels[0][x][0] = 3;
+        
+        // Fill the current position with water
+        water.create(tileX, tileY, 'tileset', 19)
+
+        // Set the bottom border
+        levels[0][x][levels[0].length - 1] = 3;
+
+        // Fill the current position with water
+        water.create(tileX, tileY + (levels[0].length - 1) * tileHeight, 'tileset', 19)
+
+    }
+    for (y = 2; y < levels[0][0].length - 2; y += 2) {
+
+        // The x position of the current tile
+        tileX = cityX;
+        
+        // The y position of the current tile
+        tileY = cityY + y * tileHeight;
+        
+        // Set the left border
+        levels[0][0][y] = 3;
+
+        // Fill the current position with water
+        water.create(tileX, tileY, 'tileset', 19)
+
+        // Set the right border
+        levels[0][0][levels[0][0].length - 1][y] = 3;
+
+        // Fill the current position with water
+        water.create(tileX + (levels[0][0].length - 1) * tileHeight, tileY, 'tileset', 19)
+    }
+
+    // Make sure that the water will be displayed on top of the street
+    game.world.bringToTop(water);
 }
